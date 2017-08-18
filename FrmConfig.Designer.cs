b3432f547a872878de9b02a85b49370fbe2134cb
OldManSpb <sanarlab@yandex.ru>
2017-08-18 17:39:10 +0300

    Add files via upload

---
+++ FrmConfig.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace Scada.Comm.Devices.KpSmsGate
+{
+    partial class FrmConfig
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEditAddressBook = new System.Windows.Forms.Button();
+            this.txtUserDisplayName = new System.Windows.Forms.TextBox();
+            this.lblUserDisplayName = new System.Windows.Forms.Label();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.lblUser = new System.Windows.Forms.Label();
+            this.txtHost = new System.Windows.Forms.TextBox();
+            this.lblHost = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnEditAddressBook
+            // 
+            this.btnEditAddressBook.Location = new System.Drawing.Point(12, 129);
+            this.btnEditAddressBook.Name = "btnEditAddressBook";
+            this.btnEditAddressBook.Size = new System.Drawing.Size(130, 23);
+            this.btnEditAddressBook.TabIndex = 11;
+            this.btnEditAddressBook.Text = "Адресная книга";
+            this.btnEditAddressBook.UseVisualStyleBackColor = true;
+            this.btnEditAddressBook.Click += new System.EventHandler(this.btnEditAddressBook_Click);
+            // 
+            // txtUserDisplayName
+            // 
+            this.txtUserDisplayName.Location = new System.Drawing.Point(12, 103);
+            this.txtUserDisplayName.Name = "txtUserDisplayName";
+            this.txtUserDisplayName.Size = new System.Drawing.Size(200, 20);
+            this.txtUserDisplayName.TabIndex = 9;
+            this.txtUserDisplayName.TextChanged += new System.EventHandler(this.txtUserDisplayName_TextChanged);
+            // 
+            // lblUserDisplayName
+            // 
+            this.lblUserDisplayName.AutoSize = true;
+            this.lblUserDisplayName.Location = new System.Drawing.Point(9, 87);
+            this.lblUserDisplayName.Name = "lblUserDisplayName";
+            this.lblUserDisplayName.Size = new System.Drawing.Size(96, 13);
+            this.lblUserDisplayName.TabIndex = 8;
+            this.lblUserDisplayName.Text = "Имя отправителя";
+            // 
+            // txtUser
+            // 
+            this.txtUser.Location = new System.Drawing.Point(12, 64);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(200, 20);
+            this.txtUser.TabIndex = 5;
+            // 
+            // lblUser
+            // 
+            this.lblUser.AutoSize = true;
+            this.lblUser.Location = new System.Drawing.Point(9, 48);
+            this.lblUser.Name = "lblUser";
+            this.lblUser.Size = new System.Drawing.Size(80, 13);
+            this.lblUser.TabIndex = 4;
+            this.lblUser.Text = "Пользователь";
+            // 
+            // txtHost
+            // 
+            this.txtHost.Location = new System.Drawing.Point(12, 25);
+            this.txtHost.Name = "txtHost";
+            this.txtHost.Size = new System.Drawing.Size(326, 20);
+            this.txtHost.TabIndex = 1;
+            // 
+            // lblHost
+            // 
+            this.lblHost.AutoSize = true;
+            this.lblHost.Location = new System.Drawing.Point(9, 9);
+            this.lblHost.Name = "lblHost";
+            this.lblHost.Size = new System.Drawing.Size(90, 13);
+            this.lblHost.TabIndex = 0;
+            this.lblHost.Text = "Адрес REST API";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(218, 64);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(120, 20);
+            this.txtPassword.TabIndex = 7;
+            this.txtPassword.UseSystemPasswordChar = true;
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(215, 48);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(45, 13);
+            this.lblPassword.TabIndex = 6;
+            this.lblPassword.Text = "Пароль";
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(185, 129);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(75, 23);
+            this.btnOK.TabIndex = 12;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.Location = new System.Drawing.Point(266, 129);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 13;
+            this.btnCancel.Text = "Отмена";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // FrmConfig
+            // 
+            this.AcceptButton = this.btnOK;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnCancel;
+            this.ClientSize = new System.Drawing.Size(350, 160);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.txtUserDisplayName);
+            this.Controls.Add(this.lblUserDisplayName);
+            this.Controls.Add(this.txtUser);
+            this.Controls.Add(this.lblUser);
+            this.Controls.Add(this.txtHost);
+            this.Controls.Add(this.lblHost);
+            this.Controls.Add(this.btnEditAddressBook);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FrmConfig";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "SMS - Конфигурация КП {0}";
+            this.Load += new System.EventHandler(this.FrmConfig_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnEditAddressBook;
+        private System.Windows.Forms.TextBox txtUserDisplayName;
+        private System.Windows.Forms.Label lblUserDisplayName;
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.Label lblUser;
+        private System.Windows.Forms.TextBox txtHost;
+        private System.Windows.Forms.Label lblHost;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
---
+++ KpSmsBlissLogic.cs
@@ -0,0 +1,356 @@
+﻿/*
+ * Copyright 2017 Mikhail Shiryaev
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * 
+ * Product  : Rapid SCADA
+ * Module   : KpEmail
+ * Summary  : Device communication logic
+ * 
+ * Author   : Mikhail Shiryaev
+ * Created  : 2016
+ * Modified : 2017
+ * 
+ * Description
+ * Sending email notifications.
+ */
+
+using Scada.Comm.Devices.AB;
+using Scada.Comm.Devices.KpSmsGate;
+using Scada.Data.Models;
+using Scada.Data.Tables;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Mail;
+using System.Threading;
+using System.Collections.Specialized;
+
+namespace Scada.Comm.Devices
+{
+    /// <summary>
+    /// Device communication logic
+    /// <para>Логика работы КП</para>
+    /// </summary>
+    public class KpSmsBlissLogic : KPLogic
+    {
+        private AB.AddressBook addressBook; // адресная книга, общая для линии связи
+        private Config config;              // конфигурация соединения с почтовым сервером
+        private bool fatalError;            // фатальная ошибка при инициализации КП
+        private string state;               // состояние КП
+        private bool writeState;            // вывести состояние КП
+
+
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        public KpSmsBlissLogic(int number)
+            : base(number)
+        {
+            CanSendCmd = true;
+            ConnRequired = false;
+            WorkState = WorkStates.Normal;
+
+            addressBook = null;
+            config = new Config();
+            
+            fatalError = false;
+            state = "";
+            writeState = false;
+
+            InitKPTags(new List<KPTag>()
+            {
+                new KPTag(1, Localization.UseRussian ? "Отправлено СМС" : "Sent SMS")
+            });
+        }
+
+
+        /// <summary>
+        /// Загрузить конфигурацию соединения с сервером
+        /// </summary>
+        private void LoadConfig()
+        {
+            string errMsg;
+            fatalError = !config.Load(Config.GetFileName(AppDirs.ConfigDir, Number), out errMsg);
+
+            if (fatalError)
+            {
+                state = Localization.UseRussian ? 
+                    "Отправка СМС уведомлений невозможна" : 
+                    "Sending notifocations is impossible";
+                throw new Exception(errMsg);
+            }
+            else
+            {
+                state = Localization.UseRussian ? 
+                    "Ожидание команд..." :
+                    "Waiting for commands...";
+            }
+        }
+
+        
+
+        /// <summary>
+        /// Попытаться получить почтовое сообщение из команды ТУ
+        /// </summary>
+        private bool TryGetMessage(Command cmd, out SmsMessage message)
+        {
+
+        
+            string cmdDataStr = cmd.GetCmdDataStr();
+            int ind1 = cmdDataStr.IndexOf(';');
+            
+
+            if (ind1 >= 0)
+            {
+                string recipient = cmdDataStr.Substring(0, ind1);
+                
+                string text = cmdDataStr.Substring(ind1 + 1);
+
+                List<string> phone_numbers = new List<string>();
+
+                if (addressBook == null)
+                {
+                    
+                    // добавление телефона получателя из данных команды
+                    phone_numbers.Add(recipient);
+                }
+                else
+                {
+                    
+                    // поиск телефонов получателей в адресной книге
+                    AB.AddressBook.ContactGroup contactGroup = addressBook.FindContactGroup(recipient);
+                    if (contactGroup == null)
+                    {
+                        AB.AddressBook.Contact contact = addressBook.FindContact(recipient);
+                        if (contact == null)
+                        {
+                            // добавление телефона получателя из данных команды
+                            phone_numbers.Add(recipient);
+                        }
+                        else
+                        {
+                            // добавление телефона получателя из контакта
+                            phone_numbers.AddRange(contact.PhoneNumbers);
+                        }
+                    }
+                    else
+                    {
+                        // добавление телефонов получателей из группы контактов
+                        foreach (AB.AddressBook.Contact contact in contactGroup.Contacts)
+                            phone_numbers.AddRange(contact.PhoneNumbers);
+                    }
+                }
+
+                // создание сообщения
+                message = CreateMessage(phone_numbers, text);
+                return message != null;
+            }
+            else
+            {
+                message = null;
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Создать SMS сообщение
+        /// </summary>
+        private SmsMessage CreateMessage(List<string> phone_nums, string text)
+        {
+            SmsMessage message = new SmsMessage();
+
+            foreach (string phone in phone_nums)
+            {
+                message.AddPhoneNo(phone);
+
+            }
+
+            if (phone_nums.Count > 0)
+            {
+                message.message = text;
+
+                return message;                
+
+            }
+            else
+            {
+                WriteToLog(string.Format(Localization.UseRussian ?
+                    "Некорректные номера телефонов получателя" :
+                    "Incorrect receivers addresses "));
+                return null;
+            }
+
+           
+        }
+
+        /// <summary>
+        /// Отправить SMS сообщение
+        /// </summary>
+        private bool SendMessage(SmsMessage message)
+        {
+
+            foreach (string phone in message.phone_numbers)
+            {
+
+                using (var webClient = new WebClient())
+                {
+                    NameValueCollection parameters = new NameValueCollection();
+
+                    parameters.Add("login", config.User);
+
+                    parameters.Add("password", config.Password);
+
+                    parameters.Add("phone", phone);
+
+                    parameters.Add("text", message.message);
+
+                    parameters.Add("sender", config.UserDisplayName);
+
+                    webClient.QueryString.Add(parameters);
+
+                    
+
+                    try
+                    {
+                        var response = webClient.DownloadString(config.Host);
+
+                        if(Localization.UseRussian)
+                            WriteToLog("Сообщение '" + message.message + "' от '" + config.UserDisplayName + "' отправлено на номер " + phone);
+                        else
+                            WriteToLog("Message '" + message.message + "' from '" + config.UserDisplayName + "' is sent to " + phone);
+
+                        if(Localization.UseRussian)
+                            WriteToLog("Ответ сервера: " + response);
+                        else
+                            WriteToLog("Server responce: " + response);
+
+                    }
+                    catch (WebException ex)
+                    {
+                        WriteToLog(ex.Message);
+                    }
+                }
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Преобразовать данные тега КП в строку
+        /// </summary>
+        protected override string ConvertTagDataToStr(int signal, SrezTableLight.CnlData tagData)
+        {
+            if (tagData.Stat > 0)
+            {
+                if (signal == 1)
+                    return tagData.Val.ToString("N0");
+            }
+
+            return base.ConvertTagDataToStr(signal, tagData);
+        }
+
+
+        /// <summary>
+        /// Выполнить сеанс опроса КП
+        /// </summary>
+        public override void Session()
+        {
+            if (writeState)
+            {
+                WriteToLog("");
+                WriteToLog(state);
+                writeState = false;
+            }
+
+            Thread.Sleep(ScadaUtils.ThreadDelay);
+        }
+
+        /// <summary>
+        /// Отправить команду ТУ
+        /// </summary>
+        public override void SendCmd(Command cmd)
+        {
+            base.SendCmd(cmd);
+            lastCommSucc = false;
+
+            if (fatalError)
+            {
+                WriteToLog(state);
+            }
+            else
+            {
+                if (cmd.CmdNum == 1 && cmd.CmdTypeID == BaseValues.CmdTypes.Binary)
+                {
+                    SmsMessage message = new SmsMessage();
+
+                    if (TryGetMessage(cmd, out message))
+                    {
+                        if (SendMessage(message))
+                            lastCommSucc = true;
+
+                        // задержка позволяет ограничить скорость отправки писем
+                        Thread.Sleep(ReqParams.Delay);
+                    }
+                    else
+                    {
+                        WriteToLog(CommPhrases.IncorrectCmdData);
+                    }
+                }
+                else
+                {
+                    WriteToLog(CommPhrases.IllegalCommand);
+                }
+
+                writeState = true;
+            }
+
+            CalcCmdStats();
+        }
+
+        /// <summary>
+        /// Выполнить действия при запуске линии связи
+        /// </summary>
+        public override void OnCommLineStart()
+        {
+            writeState = true;
+            addressBook = AbUtils.GetAddressBook(AppDirs.ConfigDir, CommonProps, WriteToLog);
+            LoadConfig();
+            SetCurData(0, 0, 1);
+        }
+
+        private class SmsMessage
+        {
+            public SmsMessage()
+            {
+               
+                phone_numbers = new List<string>();
+            }
+            public bool AddPhoneNo (string phone)
+            {
+                if (phone.StartsWith("+") && phone.Length > 10)
+                {
+                    phone_numbers.Add(phone);
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            public string message;
+            public List<string> phone_numbers;
+        }
+
+    }
+}
